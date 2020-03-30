334fa59bc3e5e38f08709ffd5657ea1f6da9b3cc
Zhiger Kalymov <jimokit.kk@gmail.com>
2020-03-30 14:08:38 +0600

    add task requrement in md

--- src/test/scala/ReverseIntegerTest.scala
+++ src/test/scala/ReverseIntegerTest.scala
@@ -4,4 +4,12 @@ class ReverseIntegerTest extends FlatSpec with Matchers {
   "ReverseInteger" should "reverse 123" in {
     ReverseInteger.reverse(123) should be (321)
   }
+
+  it should "let -123" in {
+    ReverseInteger.reverse(-123) should be (-321)
+  }
+
+  it should "let -123" in {
+    ReverseInteger.reverse(-123) should be (-321)
+  }
 }
--- src/test/scala/RomanToIntegerTest.scala
+++ src/test/scala/RomanToIntegerTest.scala
@@ -1,11 +1,23 @@
 import org.scalatest._
 
 class RomanToIntegerTest extends FlatSpec with Matchers{
-  "RomanToInteger" should "convert III to 3" in {
+  "RomanToInteger" should "let 3 from III" in {
     RomanToInteger.romanToInt("III") should be (3)
   }
 
-  it should "let IV to 4" in {
+  it should "let 4 from IV" in {
     RomanToInteger.romanToInt("IV") should be (4)
+  }
+
+  it should "let 9 from IX" in {
+    RomanToInteger.romanToInt("IX") should be (9)
+  }
+
+  it should "let 58 from LVIII" in {
+    RomanToInteger.romanToInt("LVIII") should be (58)
+  }
+
+  it should "let 1994 from MCMXCIV" in {
+    RomanToInteger.romanToInt("MCMXCIV") should be (1994)
   }
 }
