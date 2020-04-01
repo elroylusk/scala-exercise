66028ba82077c6bde9ddf247fc8dbd967d92b455
Zhiger Kalymov <jimokit.kk@gmail.com>
2020-04-01 09:18:23 +0600

    fix reverse integer test

--- src/test/scala/ReverseIntegerTest.scala
+++ src/test/scala/ReverseIntegerTest.scala
@@ -9,7 +9,7 @@ class ReverseIntegerTest extends FlatSpec with Matchers {
     ReverseInteger.reverse(-123) should be (-321)
   }
 
-  it should "let -123" in {
-    ReverseInteger.reverse(-123) should be (-321)
+  it should "let 120" in {
+    ReverseInteger.reverse(120) should be (21)
   }
 }
